e8b4e15fa7ee19905ed8d4bc0fbaaf131aec5e35
Antoine Després <contact@antoinedespres.fr>
2025-06-17 10:50:33 +0900

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Antoine Després on 17/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
