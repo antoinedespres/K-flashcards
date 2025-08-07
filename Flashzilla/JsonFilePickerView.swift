b94647d4cb1974edefe1b784881023ba660bb7f5
Antoine Després <contact@antoinedespres.fr>
2025-08-07 07:24:13 +0200

    Add remote server storing cards

---
+++ Flashzilla/JsonFilePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  JsonFilePicketView.swift
+//  Flashzilla
+//
+//  Created by Antoine Després on 20/06/2025.
+//
+
+import SwiftUI
+
+struct JsonFilePicketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JsonFilePicketView()
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+//
+//  README.md.swift
+//  Flashzilla
+//
+//  Created by Antoine Després on 07/08/2025.
+//
+
